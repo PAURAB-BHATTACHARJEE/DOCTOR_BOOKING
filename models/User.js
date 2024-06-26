bf04c5e1f3ff26932128d6662683c5a1950afb75
Paurab Bhattacharjee <104007722+PAURAB-BHATTACHARJEE@users.noreply.github.com>
2024-06-27 01:04:49 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('Users', userSchema);
+
+module.exports = User;
