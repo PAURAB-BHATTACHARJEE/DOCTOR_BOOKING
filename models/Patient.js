c53e054a58c61a2a596d31f76bca50598b806ec9
Paurab Bhattacharjee <104007722+PAURAB-BHATTACHARJEE@users.noreply.github.com>
2024-06-27 01:01:06 +0530

    Delete models directory

--- models/Patient.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PatientSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-});
-
-module.exports = mongoose.model('Patient', PatientSchema);
