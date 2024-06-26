fe92a6011bece990ceaf6e59acef14c562a18d5f
Paurab Bhattacharjee <104007722+PAURAB-BHATTACHARJEE@users.noreply.github.com>
2024-06-27 00:58:15 +0530

    Delete routes directory

--- routes/patientRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getPatients } = require('../controllers/patientController');
-const auth = require('../middlewares/authMiddleware');
-
-const router = express.Router();
-
-router.get('/', auth('doctor'), getPatients);
-
-module.exports = router;
