8bf4f607b69e63d4c98e431af05418f4e710f56a
Paurab Bhattacharjee <104007722+PAURAB-BHATTACHARJEE@users.noreply.github.com>
2024-07-01 01:02:58 +0530

    Update server.js. Adding Booking route

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
+const doctorRoutes = require('./routes/doctorRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error) => console.error('MongoDB connection error:', error));
 
 app.use('/api/users', userRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/doctors', doctorRoutes);
 
 const PORT = process.env.PORT || 5000;
 
