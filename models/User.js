21c71615f4e93b04fe5ed9a0293c6415416a76fa
Paurab Bhattacharjee <104007722+PAURAB-BHATTACHARJEE@users.noreply.github.com>
2024-08-07 15:56:48 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -27,13 +27,13 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 });
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
+// userSchema.pre('save', async function (next) {
+//     if (!this.isModified('password')) {
+//         next();
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+// });
 
 const User = mongoose.model('Users', userSchema);
 
