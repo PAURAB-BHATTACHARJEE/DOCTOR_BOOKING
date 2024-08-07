5395334c5f6ca9769e43b66997d511fabe467fdc
Paurab Bhattacharjee <104007722+PAURAB-BHATTACHARJEE@users.noreply.github.com>
2024-08-07 15:56:10 +0530

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,35 +1,14 @@
 const mongoose = require('mongoose');
 
-const bookingSchema = mongoose.Schema({
-    patientName: {
-        type: String,
-        required: true,
-    },
-    age: {
-        type: Number,
-        required: true,
-    },
-    gender: {
-        type: String,
-        required: true,
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    branch: {
-        type: String,
-        required: true,
-    },
-    doctor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Doctor',
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-    },
+const bookingSchema = new mongoose.Schema({
+    patientName: { type: String, required: true },
+    age: { type: Number, required: true },
+    gender: { type: String, required: true },
+    city: { type: String, required: true },
+    branch: { type: String, required: true },
+    doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
+    date: { type: Date, required: true },
+    email: { type: String, required: true }
 });
 
 const Booking = mongoose.model('Booking', bookingSchema);
